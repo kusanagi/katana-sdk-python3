ff3c7bf4d8606d47016fcec78744ebf477a7d0b8
Jerónimo Albi <jeronimo.albi@gmail.com>
2016-09-29 12:11:04 +0200

    Added license headers.

--- katana/errors.py
+++ katana/errors.py
@@ -1,3 +1,19 @@
+"""
+Python 3 SDK for the KATANA(tm) Platform (http://katana.kusanagi.io)
+
+Copyright (c) 2016-2017 KUSANAGI S.L. All rights reserved.
+
+Distributed under the MIT license.
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+
+"""
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2016-2017 KUSANAGI S.L. (http://kusanagi.io)"
+
+
 class KatanaError(Exception):
     """Base exception for KATANA errors."""
 

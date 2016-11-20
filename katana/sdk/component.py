5733869b3270f93fd3414316cdfd89332e9d86be
Jerónimo Albi <jeronimo.albi@gmail.com>
2016-11-20 21:41:46 +0100

    Added Component resource support.
    Resolves kusanagi/katana-sdk-python3#24.

--- katana/sdk/component.py
+++ katana/sdk/component.py
@@ -13,13 +13,66 @@ file that was distributed with this source code.
 __license__ = "MIT"
 __copyright__ = "Copyright (c) 2016-2017 KUSANAGI S.L. (http://kusanagi.io)"
 
+from ..errors import KatanaError
+
+
+class ComponentError(KatanaError):
+    """Exception calss for component errors."""
+
 
 class Component(object):
     """Base KATANA SDK component class."""
 
     def __init__(self):
+        self.__resources = {}
         self._callbacks = {}
         self._runner = None
+
+    def has_resource(self, name):
+        """Check if a resource name exist.
+
+        :param name: Name of the resource.
+        :type name: str
+
+        :rtype: bool
+
+        """
+
+        return name in self.__resources
+
+    def set_resource(self, name, callable):
+        """Store a resource.
+
+        :param name: Name of the resource.
+        :type name: str
+        :param callable: A callable that returns the resource value.
+        :type callable: function
+
+        :raises: ComponentError
+
+        """
+
+        value = callable()
+        if value is None:
+            err = 'Invalid result value for resource "{}"'.format(name)
+            raise ComponentError(err)
+
+        self.__resources[name] = value
+
+    def get_resource(self, name):
+        """Get a resource.
+
+        :param name: Name of the resource.
+        :type name: str
+
+        :rtype: object
+
+        """
+
+        if not self.has_resource(name):
+            raise ComponentError('Resource "{}" not found'.format(name))
+
+        return self.__resources[name]
 
     def run(self):
         """Run SDK component.

3118e6d62d48a62fe6e6d7f1c4120865669cccc9
Jerónimo Albi <jero@Paradox.local>
2016-11-23 09:22:54 +0100

    Finished docstrings for schema related classes.

--- katana/api/schema/param.py
+++ katana/api/schema/param.py
@@ -25,7 +25,6 @@ class ParamSchema(object):
 
         """
 
-        # TODO: Implement. Default: ''
         return ''
 
     def get_array_format(self):
@@ -43,7 +42,6 @@ class ParamSchema(object):
 
         """
 
-        # TODO: Implement. Default: 'csv'
         return 'csv'
 
     def get_pattern(self):
@@ -53,7 +51,6 @@ class ParamSchema(object):
 
         """
 
-        # TODO: Implement. Default: ''
         return ''
 
     def allow_empty(self):
@@ -63,7 +60,6 @@ class ParamSchema(object):
 
         """
 
-        # TODO: Implement. Default: False
         return False
 
     def has_default_value(self):
@@ -73,109 +69,144 @@ class ParamSchema(object):
 
         """
 
-        # TODO: Implement. Default: False
         return False
 
     def get_default_value(self):
-        """
+        """Get default value for parameter.
 
         :rtype: str
 
         """
 
+        return ''
+
     def is_required(self):
-        """
+        """Check if parameter is required.
 
         :rtype: bool
 
         """
 
     def get_items(self):
-        """
+        """Get JSON items defined for the parameter.
+
+        An empty string is returned when parameter type is not "array".
 
         :rtype: list
 
         """
 
+        return ''
+
     def get_max(self):
-        """
+        """Get maximum value for parameter.
 
         :rtype: int
 
         """
+
+        # TODO: Return maximum integer value possible for python
+        return 999999
 
     def is_exclusive_max(self):
-        """
+        """Check if max value is inclusive.
+
+        When max is not defined inclusive is False.
 
         :rtype: bool
 
         """
+
+        return False
 
     def get_min(self):
-        """
+        """Get minimum value for parameter.
 
         :rtype: int
 
         """
 
+        # TODO: Return minimum integer value possible for python
+        return -99999
+
     def is_exclusive_min(self):
-        """
+        """Check if minimum value is inclusive.
+
+        When min is not defined inclusive is False.
 
         :rtype: bool
 
         """
 
+        return False
+
     def get_max_length(self):
-        """
+        """Get max length defined for the parameter.
 
         :rtype: int
 
         """
+
+        return -1
 
     def get_min_length(self):
-        """
+        """Get minimum length defined for the parameter.
 
         :rtype: int
 
         """
+
+        return -1
 
     def get_max_items(self):
-        """
+        """Get maximum number of items allowed for the parameter.
+
+        Result is -1 When type is not "array".
 
         :rtype: int
 
         """
+
+        return -1
 
     def get_min_items(self):
-        """
+        """Get minimum number of items allowed for the parameter.
+
+        Result is -1 When type is not "array".
 
         :rtype: int
 
         """
 
+        return -1
+
     def has_unique_items(self):
-        """
+        """Check if param must contain a set of unique items.
 
         :rtype: bool
 
         """
 
     def get_enum(self):
+        """Get the set of unique values that parameter allows.
+
+        :rtype: set
+
         """
 
-        :rtype: list
-
-        """
+        return set()
 
     def get_multiple_of(self):
-        """
+        """Get value that parameter must be divisible by.
+
+        Result is -1 when this property is not defined.
 
         :rtype: int
 
         """
 
     def get_http_schema(self):
-        """
+        """Get HTTP param schema.
 
         :rtype: HttpParamSchema
 

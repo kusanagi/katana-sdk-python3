bf88c79e2daf3f37b9fdde7130d936537763fd24
Jerónimo Albi <jeronimo.albi@gmail.com>
2017-03-10 13:23:41 +0100

    Integrated pytest with setuptools

--- setup.py
+++ setup.py
@@ -20,6 +20,14 @@ setup(
         'pyzmq==15.4.0',
         'msgpack-python==0.4.7',
     ],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+        'pytest-mock',
+        'pytest-cov',
+    ],
     classifiers=[
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3.4',
